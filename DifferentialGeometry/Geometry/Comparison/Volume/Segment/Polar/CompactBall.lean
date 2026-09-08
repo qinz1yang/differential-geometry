fea3a5c22cd561d3a4c5615b06f12991f25ccdc8
Ziyang Qin <zq38@cornell.edu>
2026-09-07 21:20:46 -0400

    Express compact-ball volume in Euclidean normal and polar coordinates

--- DifferentialGeometry/Geometry/Comparison/Volume/Segment/Polar/CompactBall.lean
+++ DifferentialGeometry/Geometry/Comparison/Volume/Segment/Polar/CompactBall.lean
@@ -2,6 +2,8 @@ import DifferentialGeometry.Geometry.Exponential.MinimizingDomain.CompactBall
 import DifferentialGeometry.Geometry.Exponential.MinimizingDomain.Injectivity
 import DifferentialGeometry.Geometry.Exponential.MinimizingDomain.Measure
 import DifferentialGeometry.Analysis.Integration.Measure.Parametric.AreaFormula
+import DifferentialGeometry.Geometry.Exponential.NormalCoordinates.Integration
+import DifferentialGeometry.Analysis.Integration.Measure.Polar.Evaluation
 
 set_option autoImplicit false
 
@@ -12,7 +14,7 @@ open scoped ContDiff ENNReal Manifold Topology
 
 namespace DifferentialGeometry.Geometry.Riemannian.VolumeComparison
 
-open Exponential
+open Exponential NormalCoordinates Variation
 open DifferentialGeometry.Integral.Measure
 
 variable {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
@@ -108,5 +110,108 @@ theorem riemannianVolumeMeasure_ball_eq_lintegral_paramDensity_expMap
         riemannianVolumeMeasure (I := I) (M := M) g (F '' K) := hFK.symm
     _ ≤ riemannianVolumeMeasure (I := I) (M := M) g B :=
       measure_mono (hFL ▸ image_mono hKL)
+
+theorem riemannianVolumeMeasure_ball_eq_lintegral_curveDensity_euclideanNormalFrame
+    [PseudoEMetricSpace M] [IsRiemannianManifold I M]
+    [IsContinuousRiemannianBundle E (fun x : M => TangentSpace I x)]
+    (g : SmoothRiemannianMetric I M)
+    (hEnorm : IsMetricNorm (I := I) (M := M) g)
+    (p : M) (R : ℝ)
+    (hcpt : @IsCompact M PseudoEMetricSpace.toUniformSpace.toTopologicalSpace
+      (Metric.closedEBall p (ENNReal.ofReal R))) :
+    riemannianVolumeMeasure (I := I) (M := M) g
+        {q : M | riemannianEDist I p q < ENNReal.ofReal R} =
+      ∫⁻ w in (NormalCoordinates.euclideanNormalFrame (I := I) g p) ⁻¹'
+          (show Set E from extendibleMinimizingDomain (I := I) g p ∩ gBall (I := I) g p R),
+        ENNReal.ofReal (Variation.curveDensity (I := I) g
+          (radialCurve (I := I) g p (NormalCoordinates.euclideanNormalFrame (I := I) g p w))
+          (fun i => radialJacobiField (I := I) g p
+            (NormalCoordinates.euclideanNormalFrame (I := I) g p w)
+            (NormalCoordinates.normalBasis (I := I) g p i)) 1)
+        ∂(volume : Measure (EuclideanSpace ℝ (Fin (Module.finrank ℝ E)))) := by
+  rw [riemannianVolumeMeasure_ball_eq_lintegral_paramDensity_expMap g hEnorm p R hcpt]
+  apply lintegral_paramDensity_expMap_eq_lintegral_curveDensity_euclideanNormalFrame
+  · exact (measurableSet_extendibleMinimizingDomain (I := I) g hEnorm p).inter
+      (measurableSet_gBall (I := I) g p R)
+  · intro v hv
+    exact minimizingDomain_subset_expDomain (I := I) g p
+      (extendibleMinimizingDomain_subset_minimizingDomain (I := I) g hEnorm p hv.1)
+
+local notation "F₀" => EuclideanSpace ℝ (Fin (Module.finrank ℝ E))
+
+theorem riemannianVolumeMeasure_ball_eq_lintegral_polar_euclideanNormalFrame
+    [Nontrivial E] [PseudoEMetricSpace M] [IsRiemannianManifold I M]
+    [IsContinuousRiemannianBundle E (fun x : M => TangentSpace I x)]
+    (g : SmoothRiemannianMetric I M)
+    (hEnorm : IsMetricNorm (I := I) (M := M) g)
+    (p : M) {t A : ℝ} (htA : t ≤ A)
+    (hcpt : @IsCompact M PseudoEMetricSpace.toUniformSpace.toTopologicalSpace
+      (Metric.closedEBall p (ENNReal.ofReal A))) :
+    let L : F₀ → E := fun w => euclideanNormalFrame (I := I) g p w
+    let D : F₀ → ℝ≥0∞ := fun w => ENNReal.ofReal
+      (curveDensity (I := I) g (radialCurve (I := I) g p (L w))
+        (fun i => radialJacobiField (I := I) g p (L w) (normalBasis (I := I) g p i)) 1)
+    let T : Set F₀ := L ⁻¹'
+      (extendibleMinimizingDomain (I := I) g p ∩ gBall (I := I) g p A)
+    riemannianVolumeMeasure (I := I) (M := M) g
+        {q : M | riemannianEDist I p q < ENNReal.ofReal t} =
+      ∫⁻ u : Metric.sphere (0 : F₀) 1,
+        ∫⁻ r : Ioi (0 : ℝ) in {r : Ioi (0 : ℝ) | r.1 < t},
+          T.indicator D (r.1 • u.1)
+          ∂Measure.volumeIoiPow (Module.finrank ℝ E - 1)
+        ∂(volume : Measure F₀).toSphere := by
+  let L : F₀ → E := fun w => euclideanNormalFrame (I := I) g p w
+  let D : F₀ → ℝ≥0∞ := fun w => ENNReal.ofReal
+    (curveDensity (I := I) g (radialCurve (I := I) g p (L w))
+      (fun i => radialJacobiField (I := I) g p (L w) (normalBasis (I := I) g p i)) 1)
+  let T : Set F₀ := L ⁻¹'
+    (extendibleMinimizingDomain (I := I) g p ∩ gBall (I := I) g p A)
+  have hL : Continuous L := (euclideanNormalFrame (I := I) g p).continuous
+  have hT : MeasurableSet T :=
+    ((measurableSet_extendibleMinimizingDomain (I := I) g hEnorm p).inter
+      (measurableSet_gBall (I := I) g p A)).preimage hL.measurable
+  have hTD : MapsTo L T (expDomain (I := I) g p) := by
+    intro w hw
+    exact minimizingDomain_subset_expDomain (I := I) g p
+      (extendibleMinimizingDomain_subset_minimizingDomain (I := I) g hEnorm p hw.1)
+  have hD : AEMeasurable D ((volume : Measure F₀).restrict (T ∩ Metric.ball 0 t)) := by
+    apply ENNReal.measurable_ofReal.comp_aemeasurable
+    exact (((continuousOn_curveDensity_radialJacobiField_basis (I := I) g p
+      (normalBasis (I := I) g p)).comp hL.continuousOn hTD).mono
+        inter_subset_left).aemeasurable (hT.inter measurableSet_ball)
+  have hball (r : ℝ) : L ⁻¹' (show Set E from gBall (I := I) g p r) =
+      Metric.ball (0 : F₀) r := by
+    ext w
+    change Real.sqrt (g.inner p (euclideanNormalFrame (I := I) g p w)
+      (euclideanNormalFrame (I := I) g p w)) < r ↔ dist w 0 < r
+    rw [euclideanNormalFrame_sqrt, dist_zero_right]
+  have hpre : L ⁻¹' (extendibleMinimizingDomain (I := I) g p ∩
+      (show Set E from gBall (I := I) g p t)) = T ∩ Metric.ball (0 : F₀) t := by
+    change (L ⁻¹' extendibleMinimizingDomain (I := I) g p) ∩
+      (L ⁻¹' (show Set E from gBall (I := I) g p t)) =
+        ((L ⁻¹' extendibleMinimizingDomain (I := I) g p) ∩
+          (L ⁻¹' (show Set E from gBall (I := I) g p A))) ∩ Metric.ball (0 : F₀) t
+    rw [hball t, hball A, inter_assoc,
+      inter_eq_right.mpr (Metric.ball_subset_ball htA)]
+  have hcptt : @IsCompact M PseudoEMetricSpace.toUniformSpace.toTopologicalSpace
+      (Metric.closedEBall p (ENNReal.ofReal t)) := by
+    let : TopologicalSpace M := PseudoEMetricSpace.toUniformSpace.toTopologicalSpace
+    exact hcpt.of_isClosed_subset Metric.isClosed_closedEBall
+      (Metric.closedEBall_subset_closedEBall (ENNReal.ofReal_mono htA))
+  have hdim : Module.finrank ℝ F₀ = Module.finrank ℝ E := by simp
+  let _ : Nontrivial F₀ :=
+    Module.nontrivial_of_finrank_pos (hdim.symm ▸ Module.finrank_pos (R := ℝ) (M := E))
+  change riemannianVolumeMeasure (I := I) (M := M) g
+    {q : M | riemannianEDist I p q < ENNReal.ofReal t} = _
+  calc
+    _ = ∫⁻ w in L ⁻¹' (extendibleMinimizingDomain (I := I) g p ∩
+        (show Set E from gBall (I := I) g p t)), D w ∂(volume : Measure F₀) :=
+      riemannianVolumeMeasure_ball_eq_lintegral_curveDensity_euclideanNormalFrame
+        (I := I) g hEnorm p t hcptt
+    _ = ∫⁻ w in T ∩ Metric.ball (0 : F₀) t, D w ∂(volume : Measure F₀) := by rw [hpre]
+    _ = _ := by
+      simpa only [hdim] using
+        setLIntegral_inter_ball_polar (volume : Measure F₀) T hT t D hD
+
 
 end DifferentialGeometry.Geometry.Riemannian.VolumeComparison
