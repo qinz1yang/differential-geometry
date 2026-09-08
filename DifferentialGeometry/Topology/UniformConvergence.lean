e59fa4e93f2075c88eb39ddffb24c05238517971
Ziyang Qin <zq38@cornell.edu>
2026-09-07 20:19:00 -0400

    Add uniform convergence under varying composition

--- DifferentialGeometry/Topology/UniformConvergence.lean
+++ DifferentialGeometry/Topology/UniformConvergence.lean
@@ -1,5 +1,6 @@
 import Mathlib.Topology.UniformSpace.Compact
 import Mathlib.Topology.UniformSpace.UniformConvergence
+import Mathlib.Topology.UniformSpace.UniformConvergenceTopology
 
 set_option autoImplicit false
 
@@ -21,3 +22,18 @@ theorem eventually_mapsTo_of_tendstoUniformly
   exact hball (f x) (mem_image_of_mem f hx) (hn x)
 
 end DifferentialGeometry
+
+theorem TendstoUniformlyOn.comp_tendstoUniformly
+    {α β γ ι : Type*} [UniformSpace β] [UniformSpace γ]
+    {l : Filter ι} {s : Set β} {F : ι → β → γ} {f : β → γ}
+    {u : ι → α → β} {v : α → β}
+    (hF : TendstoUniformlyOn F f l s) (hf : UniformContinuousOn f s)
+    (hu : TendstoUniformly u v l)
+    (hus : ∀ᶠ i in l, ∀ x, u i x ∈ s) (hvs : ∀ x, v x ∈ s) :
+    TendstoUniformly (fun i x => F i (u i x)) (fun x => f (v x)) l := by
+  have hcomp := hf.comp_tendstoUniformly_eventually hus hvs hu
+  intro V hV
+  obtain ⟨W, hW, hWV⟩ := comp_mem_uniformity_sets hV
+  filter_upwards [hF W hW, hcomp W hW, hus] with i hi hci hsi
+  intro x
+  exact hWV (SetRel.prodMk_mem_comp (hci x) (hi _ (hsi x)))
